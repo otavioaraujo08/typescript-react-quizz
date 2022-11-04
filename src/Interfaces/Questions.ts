b2c36829f030faf9e603e179650390288a5e316a
otavioaraujo08 <otavioaraujo490@gmail.com>
2022-11-04 16:08:28 -0300

    feat: question & questionState type

---
+++ src/Interfaces/Questions.ts
@@ -0,0 +1,10 @@
+export type Question = {
+    category: string;
+    correct_answer: string;
+    difficulty: string;
+    incorrect_answers: string[];
+    question: string;
+    type: string;
+}
+
+export type QuestionState = Question & { answers: string[] }
